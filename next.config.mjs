5f14f2ff135e35cad0e4aceed130bb293fd143f0
sujalkyal <sujalkyal2704@gmail.com>
2025-04-26 15:19:42 +0530

    app working correctly

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+// next.config.mjs
+import { PrismaPlugin } from '@prisma/nextjs-monorepo-workaround-plugin'
 
-export default nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.plugins = [...config.plugins, new PrismaPlugin()]
+    }
+    return config
+  },
+}
+
+export default nextConfig
